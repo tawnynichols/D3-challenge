8192fa73494163459faee9871d957867ff439f4d
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-11-16 21:58:31 -0800

    adding abbr for circles

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,216 +1,138 @@
-// @TODO: YOUR CODE HERE!
 var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
     top: 20,
     right: 40,
-    bottom: 80,
+    bottom: 60,
     left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-// Create an SVG wrapper, append an SVG group that will hold our chart,
-// and shift the latter by left and top margins.
-var svg = d3
-    .select(".chart")
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-// Append an SVG group
 var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-// Initial Params
-var chosenXAxis = "age";
+d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
-// function used for updating x-scale var upon click on axis label
-function xScale(hairData, chosenXAxis) {
-    // create scales
-    var xLinearScale = d3.scaleLinear()
-        .domain([d3.min(hairData, d => d[chosenXAxis]) * 0.8,
-            d3.max(hairData, d => d[chosenXAxis]) * 1.2
-        ])
-        .range([0, width]);
+// Import Data
+d3.csv("assets/data/data.csv", function(err, hairData) {
+        if (err) throw err;
 
-    return xLinearScale;
+        console.log(hairData);
 
-}
-
-// function used for updating xAxis var upon click on axis label
-function renderAxes(newXScale, xAxis) {
-    var bottomAxis = d3.axisBottom(newXScale);
-
-    xAxis.transition()
-        .duration(1000)
-        .call(bottomAxis);
-
-    return xAxis;
-}
-
-// function used for updating circles group with a transition to
-// new circles
-function renderCircles(circlesGroup, newXScale, chosenXAxis) {
-
-    circlesGroup.transition()
-        .duration(1000)
-        .attr("cx", d => newXScale(d[chosenXAxis]));
-
-    return circlesGroup;
-}
-
-// function used for updating circles group with new tooltip
-function updateToolTip(chosenXAxis, circlesGroup) {
-
-    var label;
-
-    if (chosenXAxis === "hair_length") {
-        label = "Hair Length:";
-    } else {
-        label = "# of Albums:";
-    }
-
-    var toolTip = d3.tip()
-        .attr("class", "tooltip")
-        .offset([80, -60])
-        .html(function(d) {
-            return (`${d.rockband}<br>${label} ${d[chosenXAxis]}`);
+        // Step 1: Parse Data/Cast as numbers
+        // ==============================
+        hairData.forEach(function(data) {
+            data.smokes = +data.smokes;
+            data.age = +data.age;
+            data.poverty = +data.poverty;
         });
 
-    circlesGroup.call(toolTip);
+        // Step 2: Create scale functions
+        // ==============================
+        var xLinearScale = d3.scaleLinear()
+            .domain([20, d3.max(hairData, d => d.age)])
+            .range([0, width]);
 
-    circlesGroup.on("mouseover", function(data) {
-            toolTip.show(data);
-        })
-        // onmouseout event
-        .on("mouseout", function(data, index) {
-            toolTip.hide(data);
-        });
+        var yLinearScale = d3.scaleLinear()
+            .domain([0, d3.max(hairData, d => d.smokes)])
+            .range([height, 0]);
 
-    return circlesGroup;
-}
+        // Step 3: Create axis functions
+        // ==============================
+        var bottomAxis = d3.axisBottom(xLinearScale);
+        var leftAxis = d3.axisLeft(yLinearScale);
 
-// Retrieve data from the CSV file and execute everything below
-d3.csv("assets/data/data.csv").then(function(hairData, err) {
-    if (err) throw err;
+        // Step 4: Append Axes to the chart
+        // ==============================
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(bottomAxis);
 
-    console.log(hairData);
+        chartGroup.append("g")
+            .call(leftAxis);
 
-    // parse data
-    hairData.forEach(function(data) {
-        data.smokes = +data.smokes;
-        data.age = +data.age;
-    });
+        // Step 5: Create Circles
+        // ==============================
+        var circlesGroup = chartGroup.selectAll("circle")
+            .data(hairData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xLinearScale(d.age))
+            .attr("cy", d => yLinearScale(d.smokes))
+            .attr("r", "12")
+            .attr("stroke-width", "1")
+            .classed("stateCircle", true)
+            .attr("opacity", 0.75);
 
-    // xLinearScale function above csv import
-    var xLinearScale = xScale(hairData, chosenXAxis);
+        chartGroup.append("g")
+            .selectAll('text')
+            .data(hairData)
+            .enter()
+            .append("text")
+            .text(d => d.abbr)
+            .attr("x", d => xLinearScale(d.age))
+            .attr("y", d => yLinearScale(d.smokes))
+            .classed(".stateText", true)
+            .attr("text-anchor", "middle")
+            .attr("fill", "white")
+            .attr("font-size", "9px")
+            .attr("alignment-baseline", "central")
 
-    // Create y scale function
-    var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(hairData, d => d.smokes)])
-        .range([height, 0]);
+        // Step 6: Initialize tool tip
+        // ==============================
+        var toolTip = d3.tip()
+            .attr("class", "tooltip")
+            .offset([80, -60])
+            .style("position", "absolute")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "1px")
+            .style("border-radius", "5px")
+            .style("padding", "10px")
+            .attr("font-size", "9px")
+            .html(function(d) {
+                return (`${d.state}<br>Average Age: ${d.age}<br>Smoking %: ${d.smokes}`);
+            });
 
-    // Create initial axis functions
-    var bottomAxis = d3.axisBottom(xLinearScale);
-    var leftAxis = d3.axisLeft(yLinearScale);
+        // Step 7: Create tooltip in the chart
+        // ==============================
+        chartGroup.call(toolTip);
 
-    // append x axis
-    var xAxis = chartGroup.append("g")
-        .classed("x-axis", true)
-        .attr("transform", `translate(0, ${height})`)
-        .call(bottomAxis);
+        // Step 8: Create event listeners to display and hide the tooltip
+        // ==============================
+        circlesGroup.on("mouseover", function(data) {
+                toolTip.show(data, this);
+            })
+            // onmouseout event
+            .on("mouseout", function(data, index) {
+                toolTip.hide(data);
+            });
 
-    // append y axis
-    chartGroup.append("g")
-        .call(leftAxis);
+        // Create axes labels
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left + 40)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .attr("class", "axisText")
+            .text("Median Age of State");
 
-    // append initial circles
-    var circlesGroup = chartGroup.selectAll("circle")
-        .data(hairData)
-        .enter()
-        .append("circle")
-        .attr("cx", d => xLinearScale(d[chosenXAxis]))
-        .attr("cy", d => yLinearScale(d.smokes))
-        .attr("r", 20)
-        .attr("fill", "pink")
-        .attr("opacity", ".5");
-
-    // Create group for two x-axis labels
-    var labelsGroup = chartGroup.append("g")
-        .attr("transform", `translate(${width / 2}, ${height + 20})`);
-
-    var hairLengthLabel = labelsGroup.append("text")
-        .attr("x", 0)
-        .attr("y", 20)
-        .attr("value", "age") // value to grab for event listener
-        .classed("active", true)
-        .text("Hair Metal Ban Hair Length (inches)");
-
-    var albumsLabel = labelsGroup.append("text")
-        .attr("x", 0)
-        .attr("y", 40)
-        .attr("value", "povertyMoe") // value to grab for event listener
-        .classed("inactive", true)
-        .text("# of Albums Released");
-
-    // append y axis
-    chartGroup.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x", 0 - (height / 2))
-        .attr("dy", "1em")
-        .classed("axis-text", true)
-        .text("Number of Billboard 500 Hits");
-
-    // updateToolTip function above csv import
-    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-    // x axis labels event listener
-    labelsGroup.selectAll("text")
-        .on("click", function() {
-            // get value of selection
-            var value = d3.select(this).attr("value");
-            if (value !== chosenXAxis) {
-
-                // replaces chosenXAxis with value
-                chosenXAxis = value;
-
-                // console.log(chosenXAxis)
-
-                // functions here found above csv import
-                // updates x scale for new data
-                xLinearScale = xScale(hairData, chosenXAxis);
-
-                // updates x axis with transition
-                xAxis = renderAxes(xLinearScale, xAxis);
-
-                // updates circles with new x values
-                circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
-
-                // updates tooltips with new info
-                circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
-
-                // changes classes to change bold text
-                if (chosenXAxis === "povertyMoe") {
-                    albumsLabel
-                        .classed("active", true)
-                        .classed("inactive", false);
-                    hairLengthLabel
-                        .classed("active", false)
-                        .classed("inactive", true);
-                } else {
-                    albumsLabel
-                        .classed("active", false)
-                        .classed("inactive", true);
-                    hairLengthLabel
-                        .classed("active", true)
-                        .classed("inactive", false);
-                }
-            }
-        });
-}).catch(function(error) {
-    console.log(error);
-});
+        chartGroup.append("text")
+            .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+            .attr("class", "axisText")
+            .text("Percentage of Smoking in each State");
+    }) //.catch(function(error) {
+    //console.log(error);
+    //});
